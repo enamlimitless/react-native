67601bfe1bc40520b2119651b76e7e495c574cdf
enamulhaque7 <enamulhaquemb01@gmail.com>
2021-06-07 22:09:11 +0600

    updating

--- components/CarItem/Index.js
+++ components/CarItem/Index.js
@@ -5,12 +5,7 @@ import styles from './styles';
 
 const CarItem = (props) => {
     return (
-        <View style={styles.carContainer}>
-            
-            <ImageBackground
-            source={require('../../assets/images/Model3.jpeg')}
-            style={styles.image}
-            />
+        
             <View style={styles.titles}>
                 <Text style={styles.title}>Model X</Text>
                 <Text style={styles.subtitle}>Starting at $67,670</Text>
